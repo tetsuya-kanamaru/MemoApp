5338c1a792cf271fb480975675374f44a3ee74d2
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-09-10 12:11:35 +0900

    Replace keyboardAvoidingView with keyboardSafeView

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 // eslint-disable-next-line
-import {
-  View,
-  StyleSheet,
-  TextInput,
-  KeyboardAvoidingView,
-} from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
+import KeyboardSafeView from "../components/keyboardSafeView";
 
 export default function MemoEditScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
 
