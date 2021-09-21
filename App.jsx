2187ab641c788f5f935d0649c43b01b33134e954
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-09-21 15:41:32 +0900

    Auto login and logout button

--- App.jsx
+++ App.jsx
@@ -25,7 +25,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="SignUp"
+        initialRouteName="LogIn"
         screenOptions={{
           headerStyle: { backgroundColor: "#467fd3" },
           headerTitleStyle: { color: "#fff" },
--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // eslint-disable-next-line
 import { View, StyleSheet, TextInput, Alert } from "react-native";
 import firebase from "firebase";
@@ -12,6 +12,18 @@ export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "MemoList" }],
+        });
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   function handlePress() {
     firebase
